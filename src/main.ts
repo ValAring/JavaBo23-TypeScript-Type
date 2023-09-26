a9a70cc5318ff589e4fa52da991c1415ed35bd20
valeriearing <the.v.aring@gmail.com>
2023-09-26 16:20:29 +0200

    Added Bonus Aufgaben

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ type Student = {
     firstName: string,
     lastName: string,
     age: number,
+    subjects: subject[]
+}
+type subject = {
+    subjectName: string,
     grades: grade[]
 }
 
@@ -13,30 +17,64 @@ let antonMeier:Student = {
     firstName: "Anton",
     lastName: "Meier",
     age: 17,
-    grades: [1,4,3,1,"A",undefined,1,2]
+    subjects: [{
+        subjectName: "Sport",
+        grades: ["A", 1, undefined]
+    },{
+        subjectName: "Kunst",
+        grades: [3,2,4,5]
+    },{
+        subjectName: "Mathe",
+        grades: [1,2,"A"]
+    }]
 }
 let bertaMueller:Student = {
     firstName: "Berta",
     lastName: "Müller",
     age: 17,
-    grades: ["A",undefined,1]
+    subjects: [{
+        subjectName: "Sport",
+        grades: [1,2,1,1]
+    },{
+        subjectName: "Kunst",
+        grades: [3,"C",undefined, undefined]
+    },{
+        subjectName: "Mathe",
+        grades: [2,"A","B",undefined,2]
+    }]
 }
 let caesarSchmidt:Student = {
     firstName: "Cäsar",
     lastName: "Schmidt",
     age: 17,
-    grades: ["A",1,undefined, 3,2,4,5]
+    subjects: [{
+        subjectName: "Sport",
+        grades: ["C", 3, 4]
+    },{
+        subjectName: "Kunst",
+        grades: [2,2,4,1]
+    },{
+        subjectName: "Mathe",
+        grades: [4,5,3,"A"]
+    }]
 }
 const allStudents:Student[] = [antonMeier, bertaMueller, caesarSchmidt];
 
 function printStudent (theStudent : Student) {
-    console.log(theStudent.firstName + ' ' + theStudent.lastName + " (" + theStudent.age + ")");
-    console.log("=".repeat(30));
+    let nameString:string = theStudent.firstName + ' ' + theStudent.lastName + " (" + theStudent.age + ")";
+    console.log(nameString);
+    console.log("=".repeat(nameString.length));
+    console.log("Noten:");
 
     // Schritt 3: Erlaubt auch undefined als Wert (in der Ausgabe als *)
-    const grades = theStudent.grades.map((grade) => grade === undefined? "*" : grade);
+    //const grades = theStudent.grades.map((grade) => grade === undefined? "*" : grade);
+    theStudent.subjects.forEach((subject)=> {
+        const grades = subject.grades.map((grade) => grade === undefined? "*" : grade);
+        console.log(subject.subjectName + ": " + grades);
+    });
 
-    console.log("Noten: " + grades + "\n\n");
+    //console.log("Noten:" + grades + "\n\n");
+    console.log("\n");
 }
 
 //printStudent(antonMeier);
